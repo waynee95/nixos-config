23f14bf861eef06eaa27d0650deeb7cab1ad7be0
waynee95 <larsericmarquardt@mail.de>
2019-11-15 14:58:25 +0100

    Fix path in thinkpad config

--- machines/thinkpad_X230/configuration.nix
+++ machines/thinkpad_X230/configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ /etc/nixos/hardware-configuration.nix ../system ];
+  imports = [ /etc/nixos/hardware-configuration.nix ../../system ];
 
   boot.loader.grub.device = "/dev/sda";
 
