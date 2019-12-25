da612182bde4e290493cb7b960dddc1722d07cf6
waynee95 <larsericmarquardt@mail.de>
2019-12-25 10:36:32 +0100

    Add megatools and unrar package

--- system/default.nix
+++ system/default.nix
@@ -60,6 +60,7 @@
     fish
     gitAndTools.gitFull
     htop
+    megatools
     pandoc
     texlive.combined.scheme-medium
     pavucontrol
@@ -69,6 +70,7 @@
     termite
     tmux
     tree
+    unrar
     unzip
     vim_configurable
     wget
