3179bf3f6b096b6a26160a16dd6ac25b019e9021
waynee95 <larsericmarquardt@mail.de>
2023-04-18 18:51:31 +0200

    Rename config option

--- machines/thinkpad_X230/configuration.nix
+++ machines/thinkpad_X230/configuration.nix
@@ -11,34 +11,34 @@
   services = {
     acpid.enable = true;
 
-    libinput = {
+    xserver.libinput = {
       enable = true;
-      naturalScrolling = true;
+      touchpad.naturalScrolling = true;
     };
 
-    thinkfan = {
-      enable = true;
-      sensors = ''
-        hwmon /sys/class/thermal/thermal_zone0/temp
-      '';
-      levels = ''
-        (0, 0,  60)
-        (1, 53, 65)
-        (2, 55, 66)
-        (3, 57, 68)
-        (4, 61, 70)
-        (5, 64, 71)
-        (7, 68, 32767)
-      '';
-    };
+    # thinkfan = {
+    #   enable = true;
+    #   sensors = ''
+    #     hwmon /sys/class/thermal/thermal_zone0/temp
+    #   '';
+    #   levels = ''
+    #     (0, 0,  60)
+    #     (1, 53, 65)
+    #     (2, 55, 66)
+    #     (3, 57, 68)
+    #     (4, 61, 70)
+    #     (5, 64, 71)
+    #     (7, 68, 32767)
+    #   '';
+    # };
 
-    tlp = {
-      enable = true;
-      extraConfig = ''
-        START_CHARGE_THRESH_BAT0=67
-        STOP_CHARGE_THRESH_BAT0=100
-      '';
-    };
+    # tlp = {
+    #   enable = true;
+    #   extraConfig = ''
+    #     START_CHARGE_THRESH_BAT0=67
+    #     STOP_CHARGE_THRESH_BAT0=100
+    #   '';
+    # };
   };
 
 }
